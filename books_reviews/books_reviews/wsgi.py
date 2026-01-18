0518a0259abffcf27b2909b5575e9f0874c3c380
Matvey Spekhin <83747943+iceiceice192@users.noreply.github.com>
2026-01-18 20:40:49 +0500

    Delete books_reviews directory

--- books_reviews/books_reviews/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Указываем настройки
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
-
-# Создаем WSGI-приложение
-application = get_wsgi_application()
